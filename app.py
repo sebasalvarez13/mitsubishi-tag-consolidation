d695bd7b9d0e44040f7d9b59acb9710f29efc70a
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2022-09-27 15:29:31 -0700

    Flask app. Shows current tags and allows to update tags

---
+++ app.py
@@ -0,0 +1,44 @@
+from urllib import request
+import sqlalchemy
+import pandas as pd
+import pymysql
+from flask import Flask, render_template, request
+
+
+
+
+app = Flask(__name__)
+#syntax: engine = create_engine("mysql://USER:PASSWORD@HOST/DATABASE")
+engine = sqlalchemy.create_engine("mysql+pymysql://sebasalvarez13:BlueYeti27@localhost/tags")
+    
+@app.route('/', methods = ['GET', 'POST'])
+def tags():
+    df = pd.read_sql_table('B_complete', con = engine)
+
+    #Converts dataframe to html table    
+    tags_html = df.to_html(classes = "table table-dark table-striped", justify = 'left', index = False)
+
+    return render_template('dashboard.html', table = tags_html)  
+
+
+@app.route('/updatetag', methods = ['GET', 'POST'])
+def update_tags():
+    if request.method == 'POST':
+        new_comment = request.form['new_comment']
+        device = request.form['device']
+        query = "UPDATE B_complete SET comment = %s, status = 'Used' WHERE device_name = %s"
+
+        #Create sql connection
+        connection = engine.connect()
+
+        connection.execute(query, (new_comment, device))
+
+        print('tag updated')
+
+    return render_template('update.html') 
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
